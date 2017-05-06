5cbe8c155d30871efd242c1ae84eb4058af32597
hechty <hchw1230@qq.com>
2017-05-06 20:22:09 +0800

    用Stack类型，判断括号是否平衡

---
+++ Stack.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+# stack in python 
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
+    
+
