c481d0d3d483477694c9bbe08c4f8db24f3a240c
hechty <hchw1230@qq.com>
2017-05-13 23:49:53 +0800

    python 官方文档

---
+++ documentation/built-in-funmctions.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+
+def all(iterable):
+    for element in iterable:
+        if not element:
+            return  False
+    return True
+
+def any(iterable):
+    for element in iterabel:
+        if element:
+            return True
+    return False
