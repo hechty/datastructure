a24a35f076ba106e281a9dac5b33e2faf181615d
hechty <hchw1230@qq.com>
2017-05-13 21:21:19 +0800

    汉诺塔，参考教程实现

--- 4.10-hanoi-tower.py
+++ 4.10-hanoi-tower.py
@@ -1,23 +1,19 @@
 #! /usr/bin/env python3
+# -*-coding:utf-8-*-
 
 from Stack import Stack
 
 step = 0
-def hanoi(n):
+def hanoi(n, A, B, C):
     global step
-    if n < 2:
-        r1 = "MOVE 1 TO OTHER"
-        step += 1
-        print("STEP: ",step,"    ",end='')
-        print(r1)
+    if n < 1:
         return
     else:
-        hanoi(n-1)
+        hanoi(n-1, A, C, B)
         step += 1
-        print("STEP: ",step,"    ",end='')
-        print("Move", n,"TO B/C")
-        hanoi(n-1) 
+        print("第 {:^10d} 步: 移动 {:^4d} 号盘子到 {} 号杆".format(step,n,C))
+        hanoi(n-1, B, A, C)
         
-s = int(input("input a num:"))
-hanoi(s)
-print("Move {} disks,Using {} Step".format(s, step)) 
+s = int(input("请输入汉诺塔阶数:"))
+hanoi(s, "1", "2", "3")
+print("{:^4d} 阶汉诺塔盘,需要移动 {:^20d} 步".format(s, step)) 
