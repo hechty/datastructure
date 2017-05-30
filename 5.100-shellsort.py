75f5f61d642ac3a9b4cd586d094a914461cb533a
hechty <hchw1230@qq.com>
2017-05-30 23:06:29 +0800

    shell-insert-sort

--- 5.100-shellsort.py
+++ 5.100-shellsort.py
@@ -1,11 +1,13 @@
 #! /usr/bin/env python3
+from random import randint
+from time import clock
+
 
 def shellsort(alist):
     sublistcount = len(alist) // 2
     while sublistcount > 0:
         for startposition in range(sublistcount):
             gap_insertionsort(alist, startposition, sublistcount)
-        print("After increments of size", sublistcount, "The list is", alist)
         sublistcount = sublistcount // 2
 
 def gap_insertionsort(alist, start, gap):
@@ -18,7 +20,35 @@ def gap_insertionsort(alist, start, gap):
             position = position - gap
 
         alist[position] = currentvalu
-alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-shellsort(alist)
-print(alist)
 
+
+def insertion_sort(alist):
+    orded_list = alist[:1]
+    for i in range(len(alist)-1):
+        new_data = alist[i+1]
+        orded_list = insertion(new_data, orded_list)
+        
+    return orded_list
+
+def insertion(new_data, orded_list):
+    posi = 0
+    for i in range(len(orded_list) - 1, -1, -1):
+        if new_data >= orded_list[i]:
+            posi = i + 1
+            break
+    orded_list.insert(posi, new_data)
+    return orded_list
+alist = [randint(0,3000) for _ in range(1000)]
+blist = [randint(0,3000) for _ in range(1000)]
+start = clock()
+for i in range(10):
+    insertion_sort(alist)
+end = clock()
+print("insertion_sort runningtime:",end-start)
+print(alist[:15])
+start = clock()
+for i in range(10):
+    shellsort(blist)
+end = clock()
+print("shell_sort runningtime:", end - start)
+print(blist[:15])
