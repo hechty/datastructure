85190409395e3442068658166857eb502609e6cf
hechty <hchw1230@qq.com>
2017-06-02 23:17:13 +0800

    list tree

---
+++ 6.4-listtree.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+
+myTree = ['a', ['b',['d',[],[]],['e',[],[]]],['c',['f',[],[]],[]]]
+print(myTree)
+print('left subtree = ', myTree[1])
+print('root = ',myTree[0])
+print('right subtree = ', myTree[2])
+
